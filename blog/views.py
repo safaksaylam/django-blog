a1b203370c0a5fc5d988644c228c78931806169b
Safak Saylam <saylamsafak@gmail.com>
2016-10-29 02:51:24 +0300

    Frontend giriş yapıldı.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+    url(r'', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
