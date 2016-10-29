d4f93635854cc44d387707f08799b29b621ea5a9
Safak Saylam <saylamsafak@gmail.com>
2016-10-29 14:44:57 +0300

    Frontend post güncellemesi v1.1

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(yayinlama_tarihi__lte=timezone.now()).order_by('yayinlama_tarihi')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
