4d25d261df365c97085eed228bfb5399142e9e9a
Safak Saylam <saylamsafak@gmail.com>
2016-10-30 14:35:59 +0300

    Frontend yazı görünümleri eklendi.

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(yayinlama_tarihi__lte=timezone.now()).order_by('yayinlama_tarihi')
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post' : post})
